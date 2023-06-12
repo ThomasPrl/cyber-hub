6d7a27d44eeb8ca10497015aa388fd81991015c4
Thomas PEREL <thomas.perel@telecom-sudparis.eu>
2023-06-12 16:14:45 +0200

    Ajout d'une page d'erreur 404

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import {Route, Routes} from "react-router-dom"
 
 import Home from './components/Home';
-import SixG from './components/SixG.tsx';
 import Article from './components/Article.tsx';
 import Conferences from './components/Conferences';
 import Certifications from './components/Certifications';
 import About from './components/About'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Error from './components/Error';
 
 
 function App() {
@@ -22,6 +22,7 @@ function App() {
             <Route path="/conferences" element={<Conferences />} />
             <Route path="/certifications" element={<Certifications />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error/>}/>
       </Routes>
 
       <Footer />
---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Error() {
+    return (
+        <div class="flex justify-center items-center mt-60">
+            <p class="font-bold text-center text-4xl">
+                ERROR 404 - PAGE NOT FOUND
+            </p>
+        </div>
+
+    )
+}
+
+export default Error
